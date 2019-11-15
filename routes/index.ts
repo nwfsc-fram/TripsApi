87f22e35265ce161f51441a5fb0b27b47a7c45d5
Seth Gerou <seth.gerou@noaa.gov>
2019-11-15 13:38:47 -0800

    route functions implemented

--- routes/index.ts
+++ routes/index.ts
@@ -7,24 +7,38 @@ const masterDev = couchDB.db.use('master-dev')
 
 const getTrips = (req, res) => {
     console.log(req.query);
-    masterDev.view('TripsApi', 'all_api_trips', {"reduce": false, "descending": true}).then((body) => {
-        res.json(body)
+    masterDev.view('TripsApi', 'all_api_trips', {"reduce": false, "descending": true, include_docs: true}).then((body) => {
+        if (body.rows) {
+            const docs = body.rows.map((row) => row.doc)
+            switch(Object.keys(req.query)[0]) {
+                case 'vesselId':
+                  res.json(docs.filter( (doc) => doc.vesselId === req.query.vesselId ))
+                  break;
+                case 'captain':
+                    res.json(docs.filter( (doc) => doc.captain.toLowerCase() === req.query.captain.toLowerCase() ))
+                    break;
+                case 'port':
+                    res.json(docs.filter( (doc) => doc.departurePort.toLowerCase() === req.query.port.toLowerCase() || doc.returnPort.toLowerCase() === req.query.port.toLowerCase() ))
+                    break;
+                case 'fishery':
+                    res.json(docs.filter( (doc) => doc.fisheries && doc.fisheries.map( (fishery) => fishery.toLowerCase() ).includes(req.query.fishery) ))
+                    break;
+                default:
+                    res.json(docs)
+              }
+        } else {
+            res.status(400).send('not found')
+        }
       });
-    // res.json(['trip 1', 'trip 2', 'trip 3']);
 }
 
 const newTrip = (req, res) => {
-    // get next tripId
     masterDev.view('TripsApi', 'all_api_trips', {"reduce": false, "descending": true, "limit": 1}).then((body) => {
         const maxId = body.rows[0].key
-        // apply tripId to req.body
         const newTrip = req.body
         newTrip.tripId = maxId + 1
-        console.log(newTrip)
-        // insert into db
-        masterDev.insert(newTrip).then(
+        masterDev.bulk({docs: [newTrip]}).then(
             setTimeout(() => {
-
                 masterDev.view('TripsApi', 'all_api_trips', {"reduce": false, "key": maxId + 1}).then((result) => {
                     res.send(result)
                 })
@@ -34,16 +48,21 @@ const newTrip = (req, res) => {
 }
 
 const getTrip = (req, res) => {
-    console.log(req.params);
     masterDev.view('TripsApi', 'all_api_trips', {"reduce": false, "key": parseInt(req.params.tripId), "include_docs": true}).then((body) => {
         res.json(body.rows[0].doc);
     })
 }
 
-const updateTrip = (req, res) => {
-    console.log(req.params);
-    console.log(req.body);
-    res.json('updated trip');
+const updateTrip = async (req, res) => {
+    const existing = await masterDev.get(req.body._id)
+    console.log(existing)
+    if (existing.tripId === req.body.tripId ) {
+        masterDev.bulk({docs: [req.body]}).then( (body) => {
+            res.json(body);
+        })
+    } else {
+        res.status(500).send('Trip ID can not be changed.')
+    }
 }
 
 const API_VERSION = 'v1';
