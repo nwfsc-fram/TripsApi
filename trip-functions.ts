714615b81b9e5d849ac49b5f563c612a6b97d976
Seth Gerou <seth.gerou@noaa.gov>
2020-08-25 14:57:29 -0700

    em: move unsorted catch to bn-expansions

--- trip-functions.ts
+++ trip-functions.ts
@@ -52,57 +52,10 @@ export async function catchEvaluator(tripNum: string) {
             fishTickets.push(fishTickets, await getFishTicket(row.fishTicketNumer))
         }
 
-        function unsortedCatch(thirdPartyReview: any, fishTickets: any) { // calc performed per haul
-            let catches: any[] = jp.query(thirdPartyReview, '$..catch');
-            catches = flattenDeep(catches);
-
-            let results = [];
-
-            for (const haul of thirdPartyReview.hauls) {
-                let haulCatches = jp.query(haul, '$..catch');
-                const unsortedCatch = haulCatches.reduce( (acc, val) => {
-                    if (val.speciesCode === 'UNST') {
-                        return acc + val.weight;
-                    } else {
-                        return acc;
-                    }
-                }, 0)
-
-                // get total catch weight from fish tickets
-                let totalWeight = fishTickets.reduce((acc: number, val: any) => {
-                    return acc + val.LANDED_WEIGHT_LBS
-                }, 0)
-
-                // get unique species from fish tickets
-                const specieses = uniq(fishTickets.map( (row: any) => row.PACFIN_SPECIES_CODE))
-
-                // get sum of landed lbs, percent of total, and calculated net bleed lbs per species
-                const speciesWeights = [];
-                for (const species of specieses) {
-                    const speciesWeight = fishTickets.reduce((acc: number, val: any) => {
-                        if (val.PACFIN_SPECIES_CODE === species) {
-                            return acc + val.LANDED_WEIGHT_LBS
-                        } else {
-                            return acc
-                        }
-                    }, 0)
-                    const percent_of_total = speciesWeight / totalWeight;
-                    const net_bleed_weight = percent_of_total * unsortedCatch;
-                    results.push( // should these be calculated per haul, or for the whole trip?
-                        {
-                            PACFIN_SPECIES_CODE: species,
-                            LANDED_WEIGHT_SUM: speciesWeight,
-                            PERCENT_OF_TOTAL: percent_of_total,
-                            NET_BLEED_WEIGHT: net_bleed_weight,
-                            haulNum: haul.haulNum
-                        });
-                }
-            }
-            console.log(results);
-            return results;
-        }
-
-        unsortedCatch(thirdParty, fishTickets);
+        // Calculation for any fish submitted without a weight.
+        // Lost gear
+        // Apportioning data for a grouping (example THDS)
+        // NonIFQ species
 
         // evaluate catch docs
         // is source logbook, thirdParty, or nwfsc
