ef67cc933289a9154a838ebbc15252d683a424ca
Melina <44685558+melinashak-noaa@users.noreply.github.com>
2020-09-18 16:35:52 -0700

    selecting debitSource

--- util/formatter.ts
+++ util/formatter.ts
@@ -1,7 +1,11 @@
-import { Catches, CatchResults, ResponseCatchTypeName } from '@boatnet/bn-models';
-import { set, get } from 'lodash';
+import { Catches, CatchResults, ResponseCatchTypeName, Disposition, sourceType } from '@boatnet/bn-models';
+import { set, get, uniqBy } from 'lodash';
 
-export function format(logbook: Catches, review: Catches, audit: Catches) :CatchResults {
+const dbConfig = require('../dbConfig.json').dbConfig;
+const couchDB = require('nano')(dbConfig.login);
+const masterDev = couchDB.db.use('master-dev');
+
+export async function format(logbook: Catches, review: Catches, audit: Catches) {
     let result: CatchResults = {
         type: ResponseCatchTypeName,
         tripNum: logbook.tripNum,
@@ -10,10 +14,27 @@ export function format(logbook: Catches, review: Catches, audit: Catches) :Catch
     const logbookCatch: any[] = catchToHaul(logbook);
     const reviewCatch: any[] = catchToHaul(review);
     const auditCatch: any[] = catchToHaul(audit);
-
     set(result, 'logbookCatch', logbookCatch);
     set(result, 'thirdPartyReviewCatch', reviewCatch);
     set(result, 'nwfscAuditCatch', auditCatch);
+
+    const ifqLogbookCatchHaulLevel = await setIFQHaulLevelData(result.logbookCatch);
+    const ifqThirdPartyReviewCatchHaulLevel = await setIFQHaulLevelData(result.thirdPartyReviewCatch);
+    const ifqNwfscAuditHaulLevel = await setIFQHaulLevelData(result.nwfscAuditCatch);
+    set(result, 'ifqLogbookCatchHaulLevel', ifqLogbookCatchHaulLevel);
+    set(result, 'ifqThirdPartyReviewCatchHaulLevel', ifqThirdPartyReviewCatchHaulLevel);
+    set(result, 'ifqNwfscAuditHaulLevel', ifqNwfscAuditHaulLevel);
+
+    const ifqLogbookTripLevel = await setTripLevelData(result.ifqLogbookCatchHaulLevel);
+    const ifqThirdPartyReviewTripLevel = await setTripLevelData(result.ifqThirdPartyReviewCatchHaulLevel);
+    const ifqNwfscAuditTripLevel = await setTripLevelData(result.ifqNwfscAuditHaulLevel);
+    set(result, 'ifqLogbookTripLevel', ifqLogbookTripLevel);
+    set(result, 'ifqThirdPartyReviewTripLevel', ifqThirdPartyReviewTripLevel);
+    set(result, 'ifqNwfscAuditTripLevel', ifqNwfscAuditTripLevel);
+
+    const ifqTripReporting = await setIFQTripReporting(result);
+    set(result, 'ifqTripReporting', ifqTripReporting);
+
     return result;
 }
 
@@ -29,6 +50,8 @@ function catchToHaul(catchVals: Catches) {
                 weight: catchVal.weight,
                 count,
                 speciesCode: catchVal.speciesCode,
+                wcgopSpeciesCode: catchVal.wcgopSpeciesCode, // TODO reference view to populate this
+                // TODO populate docId from view (the field beth requested)
                 startDepth: haul.startDepth,
                 startLatitude: haul.startLatitude,
                 startLongitude: haul.startLongitude,
@@ -44,4 +67,112 @@ function catchToHaul(catchVals: Catches) {
         }
     }
     return results;
+}
+
+async function setIFQHaulLevelData(catchResults: any[]) {
+    // get ifq grouping name for each record
+    for (let i = 0; i < catchResults.length; i++) {
+        const ifqGrouping = await masterDev.view('Ifq', 'wcgop-codes-to-ifq-grouping',
+            { "key": catchResults[i].wcgopSpeciesCode, "include_docs": false });
+        set(catchResults[i], 'ifqGrouping', ifqGrouping.rows[0].value);
+    }
+
+    // agg at haul level by ifqGrouping and disposition
+    const uniqHauls = uniqBy(catchResults, (catchResult) => {
+        return catchResult.ifqGrouping + catchResult.disposition + catchResult.haulNum
+    })
+    const resultHauls = [];
+    for (const haul of uniqHauls) {
+        let initWeight = 0;
+        const grouping = catchResults.filter((haulVal) =>
+            haulVal.ifqGrouping === haul.ifqGrouping && haulVal.disposition === haul.disposition && haulVal.haulNum === haul.haulNum
+        );
+        const totalWeight = grouping.reduce((accumulator, currentValue) => {
+            if (typeof currentValue.weight === 'number') {
+                return accumulator + currentValue.weight;
+            }
+        }, initWeight);
+        resultHauls.push({
+            ifqGrouping: haul.ifqGrouping,
+            disposition: haul.disposition,
+            weight: totalWeight,
+            haulNum: haul.haulNum
+        })
+    }
+    return resultHauls;
+}
+
+function setTripLevelData(catchResults: any[]) {
+    const ifqDispositionGroup = uniqBy(catchResults, (catchResult) => {
+        return catchResult.ifqGrouping + catchResult.disposition
+    })
+    const tripLevelData = [];
+    for (const group of ifqDispositionGroup) {
+        let initWeight = 0;
+        const grouping = catchResults.filter((haulVal) =>
+            haulVal.ifqGrouping === group.ifqGrouping && haulVal.disposition === group.disposition
+        );
+        const totalWeight = grouping.reduce((accumulator, currentValue) => {
+            if (typeof currentValue.weight === 'number') {
+                return accumulator + currentValue.weight;
+            }
+        }, initWeight);
+        tripLevelData.push({
+            ifqGrouping: group.ifqGrouping,
+            disposition: group.disposition,
+            weight: totalWeight
+        })
+    }
+    return tripLevelData;
+}
+
+function setIFQTripReporting(catchResult: CatchResults) {
+    const resultsArr: any[] = [];
+
+    let allRecords = catchResult.ifqLogbookTripLevel.concat(catchResult.ifqThirdPartyReviewTripLevel);
+    
+    const ifqGroupings = uniqBy(allRecords, (record: any) => {
+        return record.ifqGrouping + record.disposition
+    })
+    for(const grouping of ifqGroupings) {
+        if (grouping.disposition === Disposition.DISCARDED) {
+            const emCatch = catchResult.ifqThirdPartyReviewTripLevel.filter((catchVal) => {
+                if (grouping.ifqGrouping === catchVal.ifqGrouping && catchVal.disposition === Disposition.DISCARDED) {
+                    return catchVal;
+                }
+            });
+            const logbookCatch = catchResult.ifqLogbookTripLevel.filter((catchVal) => {
+                if (grouping.ifqGrouping === catchVal.ifqGrouping && catchVal.disposition === Disposition.DISCARDED) {
+                    return catchVal;
+                }
+            });
+            const debitSource = selectDebitSource(emCatch[0], logbookCatch[0]);
+            resultsArr.push(debitSource);
+        } else {
+            resultsArr.push(grouping);
+        }
+    }
+    return resultsArr;
+}
+
+// TODO still need to implemented all the logic as specified in business rules
+function selectDebitSource(emCatch, logbookCatch) {
+    const emWeight = emCatch && emCatch.weight ? emCatch.weight : 0;
+    const logbookWeight = logbookCatch && logbookCatch.weight ? logbookCatch.weight : 0;
+
+    const difference = Math.abs(emWeight - logbookWeight);
+    const tenPercentOfEM = .1 * emWeight;
+
+    // based off EM business rules
+    if (emWeight === 0) {
+        return logbookCatch;
+    } else if (logbookWeight === 0) {
+        return emCatch;
+    } else if (emWeight > 0 && logbookWeight > 0) {
+        if (difference <= tenPercentOfEM) {
+            return logbookCatch;
+        } else {
+            return emWeight > logbookWeight ? emCatch : logbookCatch;
+        }
+    }
 }
--- util/trip-functions.ts
+++ util/trip-functions.ts
@@ -2,10 +2,10 @@ const dbConfig = require('../dbConfig.json').dbConfig;
 const couchDB = require('nano')(dbConfig.login);
 const masterDev = couchDB.db.use('master-dev');
 const jp = require('jsonpath');
-import { cloneDeep, flattenDeep, set, uniq } from 'lodash';
+import { cloneDeep, flattenDeep, get, set, uniq } from 'lodash';
 import { getFishTicket } from './oracle_routines';
-import { unsortedCatch, lostCodend, selectiveDiscards } from '@boatnet/bn-expansions';
-import { Catches, sourceType } from '@boatnet/bn-models';
+import { unsortedCatch, lostCodend, selectiveDiscards, ExpansionParameters, discardMortalityRates } from '@boatnet/bn-expansions';
+import { Catches, sourceType, Disposition, GearTypes } from '@boatnet/bn-models';
 import { format } from './formatter';
 import * as moment from 'moment';
 
@@ -28,8 +28,8 @@ export async function catchEvaluator(tripNum: string) {
         let fishTickets = [];
 
         // get catch docs
-        await masterDev.view('TripsApi', 'all_api_catch', {"reduce": false, "key": parseInt(tripNum, 10), "include_docs": true}).then((body) => {
-            if ( body.rows.length > 0) {
+        await masterDev.view('TripsApi', 'all_api_catch', { "reduce": false, "key": parseInt(tripNum, 10), "include_docs": true }).then((body) => {
+            if (body.rows.length > 0) {
                 const docs = body.rows.map((row) => row.doc);
                 for (const doc of docs) {
                     if (doc.source === 'logbook') {
@@ -49,29 +49,30 @@ export async function catchEvaluator(tripNum: string) {
             if (logbook) {
                 if (logbook.fishTickets) {
                     for (const row of logbook.fishTickets) {
-                        fishTickets.push.apply(fishTickets, await getFishTicket(row.fishTicketNumber));
+                        //    fishTickets.push.apply(fishTickets, await getFishTicket(row.fishTicketNumber));
                     }
                 }
-                logbook = cloneDeep( await evaluateTripCatch(logbook));
+                logbook = cloneDeep(await evaluateTripCatch(logbook));
             }
             if (thirdParty) {
-                thirdParty = cloneDeep( await evaluateTripCatch(thirdParty));
+                thirdParty = cloneDeep(await evaluateTripCatch(thirdParty));
             }
             if (nwfscAudit) {
-                nwfscAudit = cloneDeep( await evaluateTripCatch(nwfscAudit));
+                nwfscAudit = cloneDeep(await evaluateTripCatch(nwfscAudit));
             }
             // then write all tripCatch docs to results doc
-            let result: any = format(logbook, thirdParty, nwfscAudit);
+            let result: any = await format(logbook, thirdParty, nwfscAudit);
             const existingDoc = await masterDev.view('TripsApi', 'expansion_results',
-                                                   { "key": tripNum, "include_docs": true });
+                { "key": tripNum, "include_docs": true });
+
             if (existingDoc.rows.length !== 0) {
-              let currDoc = existingDoc.rows[0].doc;
-              set(result, 'revisionHistory', updateRevisionHistory(currDoc));
-              set(result, '_id', currDoc._id);
-              set(result, '_rev', currDoc._rev);
-              set(result, 'updateDate', moment().format());
+                let currDoc = existingDoc.rows[0].doc;
+                set(result, 'revisionHistory', updateRevisionHistory(currDoc));
+                set(result, '_id', currDoc._id);
+                set(result, '_rev', currDoc._rev);
+                set(result, 'updateDate', moment().format());
             } else {
-              set(result, 'createDate', moment().format());
+                set(result, 'createDate', moment().format());
             }
             await masterDev.bulk({ docs: [result] });
         } catch (err) {
@@ -81,66 +82,72 @@ export async function catchEvaluator(tripNum: string) {
         async function evaluateTripCatch(tripCatch: Catches) {
             let flattenedCatch: any[] = jp.query(tripCatch, '$..catch');
             flattenedCatch = flattenDeep(flattenedCatch);
+            let expansionParams: ExpansionParameters = { currCatch: tripCatch };
 
             // does any catch have a length and or a count but not a weight?
-            if (flattenedCatch.find( (row: any) => (row.length || row.count) && !row.weight)) {
+            if (flattenedCatch.find((row: any) => (row.length || row.count) && !row.weight)) {
                 console.log('length or count without weight found.');
                 //tripCatch = weightFromLengthOrCount(tripCatch);
             }
 
             // does catch contain pacific halibut, lingcod, or sablefish?
-            if (flattenedCatch.find ((row: any) => ['PHLB', '101', 'LCOD', '603', 'SABL', '203'].includes(row.speciesCode.toString()))) {
+            if (flattenedCatch.find((row: any) => ['PHLB', '101', 'LCOD', '603', 'SABL', '203'].includes(row.speciesCode.toString()))) {
                 console.log('mortality rate species found');
-                // tripCatch = mortalityRateCalc(tripCatch);
+                const dmr: discardMortalityRates = new discardMortalityRates();
+                expansionParams = { currCatch: tripCatch };
+                tripCatch = cloneDeep(dmr.expand(expansionParams));
             };
 
             // is any catch unsorted catch? ('UNST' or '999' speciesCode) (Net Bleed)?
             if (
-                flattenedCatch.find( (row: any) => ['UNST', '999'].includes(row.speciesCode.toString())) &&
-                tripCatch.hauls.find( (row: any) => ['1', '2', '3', '4', '5'].includes(row.gearTypeCode))
+                flattenedCatch.find((row: any) => ['UNST', '999'].includes(row.speciesCode.toString())) &&
+                tripCatch.hauls.find((row: any) => ['1', '2', '3', '4', '5'].includes(row.gearTypeCode))
             ) {
                 console.log('unsorted catch (net bleed) found');
                 const unsortedCatchExp: unsortedCatch = new unsortedCatch();
-                tripCatch = cloneDeep(unsortedCatchExp.rulesExpansion(tripCatch, fishTickets));
+                expansionParams = { currCatch: tripCatch, fishTickets };
+                tripCatch = cloneDeep(unsortedCatchExp.expand(expansionParams));
             }
 
             // any fixed-gear haul have lost gear (gearLost > 0 )?
-            if (tripCatch.hauls.find( (row: any) => row.gearLost && row.gearLost > 0 && ['10', '19', '20'].includes(row.gearTypeCode)) ) {
+            if (tripCatch.hauls.find((row: any) => row.gearLost && row.gearLost > 0 && ['10', '19', '20'].includes(row.gearTypeCode))) {
                 console.log('lost fixed gear found');
                 //tripCatch = lostFixedGear(tripCatch);
             }
 
             // any haul have lost codend (isCodendLost = true)?
-            if (tripCatch.hauls.find( (row: any) => row.isCodendLost && ['1', '2', '3', '4', '5'].includes(row.gearTypeCode))) {
+            if (tripCatch.hauls.find((row: any) => row.isCodendLost && ['1', '2', '3', '4', '5'].includes(row.gearTypeCode))) {
                 console.log('lost trawl gear codend found');
                 const lostCodendExp: lostCodend = new lostCodend();
-                tripCatch = cloneDeep(lostCodendExp.logbookExpansion(tripCatch));
+                expansionParams = { currCatch: tripCatch };
+                tripCatch = cloneDeep(lostCodendExp.expand(expansionParams));
             }
 
             // any review/audit catch in a general grouping that needs to be expanded to specific members?
-            if (flattenedCatch.find( (row: any) => ['5000'].includes(row.speciesCode.toString())) && ['thirdParty', 'nwfscAudit'].includes(tripCatch.source)) {
+            if (flattenedCatch.find((row: any) => ['5000'].includes(row.speciesCode.toString())) && ['thirdParty', 'nwfscAudit'].includes(tripCatch.source)) {
                 console.log('review general grouping found');
                 const selectiveDiscardsExp: selectiveDiscards = new selectiveDiscards();
-                tripCatch = cloneDeep(selectiveDiscardsExp.rulesExpansion(logbook, tripCatch));
+                expansionParams = { currCatch: tripCatch, logbook };
+                tripCatch = cloneDeep(selectiveDiscardsExp.expand(expansionParams));
             }
-         
-          return tripCatch;
+
+            return tripCatch;
         }
 
         // evaluate catch docs
-            // logbook
-                // any catch have a length and or a count but not a weight?  if true, perform weight from length/weight calcs
-                    // then pacific halibut (and lingcod and sablefish) - also mortality rate calc (dmr)
-                // is any catch unsorted catch? ('UNST' or 999 speciesCode) (Net Bleed) perform unsorted catch calcs - do we also check fishery or gearType (any trawl fishery (not pot, H&l or longline))
-                // any haul have lost gear (gearLost > 0 ) for fixed-gear fisheries perform lost pots or hooks calcs (same lost pots calc)
-                // any haul have lost codend (isCodendLost = true) - perform lost codend cals
-            // review and audit
-                // any catch have a length and or a count but not a weight?  if true, perform length/weight calcs
-                    // then pacific halibut (and lingcod and sablefish) - also mortality rate calc (dmr)
-                // is any catch unsorted catch? ('UNST' or 999 speciesCode) (Net Bleed) perform unsorted catch calcs - do we also check fishery or gearType (any trawl fishery (not pot, H&l or longline))
-                // any haul have lost gear (gearLost > 0 ) for fixed-gear fisheries perform lost pots or hooks calcs (same lost pots calc)
-                // any haul have lost codend (isCodendLost = true) - perform lost codend cals
-                // any catch in a general grouping that needs to be expanded to specific members? - perform selective discards calcs
+        // logbook
+        // any catch have a length and or a count but not a weight?  if true, perform weight from length/weight calcs
+        // then pacific halibut (and lingcod and sablefish) - also mortality rate calc (dmr)
+        // is any catch unsorted catch? ('UNST' or 999 speciesCode) (Net Bleed) perform unsorted catch calcs - do we also check fishery or gearType (any trawl fishery (not pot, H&l or longline))
+        // any haul have lost gear (gearLost > 0 ) for fixed-gear fisheries perform lost pots or hooks calcs (same lost pots calc)
+        // any haul have lost codend (isCodendLost = true) - perform lost codend cals
+        // review and audit
+        // any catch have a length and or a count but not a weight?  if true, perform length/weight calcs
+        // then pacific halibut (and lingcod and sablefish) - also mortality rate calc (dmr)
+        // is any catch unsorted catch? ('UNST' or 999 speciesCode) (Net Bleed) perform unsorted catch calcs - do we also check fishery or gearType (any trawl fishery (not pot, H&l or longline))
+        // any haul have lost gear (gearLost > 0 ) for fixed-gear fisheries perform lost pots or hooks calcs (same lost pots calc)
+        // any haul have lost codend (isCodendLost = true) - perform lost codend cals
+        // any catch in a general grouping that needs to be expanded to specific members? - perform selective discards calcs
 
         console.log('catch evaluated!!!');
     }, 3000
