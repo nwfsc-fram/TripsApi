7bae01a663ca5c782d2169cef1e94c9680190130
Melina <44685558+melinashak-noaa@users.noreply.github.com>
2020-11-23 16:55:55 -0800

    making use of speciesCodeLookup and getting counts for fish tickets (#121)

--- util/trip-functions.ts
+++ util/trip-functions.ts
@@ -16,7 +16,7 @@ export async function catchEvaluator(tripNum: number) {
         const speciesCodeLookup = {};
         for (const row of codesQuery.rows) {
             speciesCodeLookup[row.key] = {
-                translatedCode: row.value[0].toString(),
+                translatedCode: row.value[0] ? row.value[0].toString() : '',
                 isWcgopEmPriority: row.value[1],
                 isProtected: row.value[2]
             };
@@ -56,6 +56,10 @@ export async function catchEvaluator(tripNum: number) {
                 console.log('not found');
             }
         })
+
+        // any review/audit catch in a general grouping that needs to be expanded to specific members?
+        const mixedGroupings: any = await getMixedGroupingInfo();
+        const mixGroupingKeys: string[] = Object.keys(mixedGroupings);
 
         try {
             if (logbook) {
@@ -118,7 +122,7 @@ export async function catchEvaluator(tripNum: number) {
             if (flattenedCatch.find((row: any) => ['PHLB', '101', 'LCOD', '603', 'SABL', '203'].includes(row.speciesCode.toString()))) {
                 console.log('discard mortality rate species found');
                 const dmr: discardMortalityRates = new discardMortalityRates();
-                currCatch = cloneDeep(dmr.expand({ currCatch }));
+                currCatch = cloneDeep(dmr.expand({ currCatch, speciesCodeLookup }));
             };
 
             // is any catch unsorted catch? ('UNST' or '999' speciesCode) (Net Bleed)?
@@ -142,16 +146,13 @@ export async function catchEvaluator(tripNum: number) {
             if (currCatch.hauls.find((row: any) => row.isCodendLost && row.gear === 'trawl')) {
                 console.log('lost trawl gear codend found');
                 const lostCodendExp: lostCodend = new lostCodend();
-                currCatch = cloneDeep(lostCodendExp.expand({ currCatch }));
+                currCatch = cloneDeep(lostCodendExp.expand({ currCatch, speciesCodeLookup }));
             }
 
-            // any review/audit catch in a general grouping that needs to be expanded to specific members?
-            const mixedGroupings: any = await getMixedGroupingInfo();
-            const mixGroupingKeys: string[] = Object.keys(mixedGroupings);
             if (flattenedCatch.find((row: any) => mixGroupingKeys.includes(row.speciesCode.toString())) && ['thirdParty', 'nwfscAudit'].includes(currCatch.source)) {
                 console.log('selective discards found');
                 const selectiveDiscardsExp: selectiveDiscards = new selectiveDiscards();
-                currCatch = cloneDeep(selectiveDiscardsExp.expand({ currCatch, logbook, mixedGroupings }));
+                currCatch = cloneDeep(selectiveDiscardsExp.expand({ currCatch, logbook, mixedGroupings, speciesCodeLookup }));
             }
             return currCatch;
         }
