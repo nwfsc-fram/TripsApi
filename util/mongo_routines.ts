512c086ee51f01ceb095b5f76ebad729f0fd7452
Seth Gerou <seth.gerou@noaa.gov>
2021-03-05 11:43:42 -0800

    addMongo: update and delete + enhanced read

--- util/mongo_routines.ts
+++ util/mongo_routines.ts
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+import { cloneDeep } from 'lodash';
 
 const mongoUri = require('../dbConfig.json').mongoUri;
 const mongoDbName = 'common';
@@ -11,7 +12,17 @@ export async function findDocuments(collectionName, callback, query?) {
 
         const collection = db.collection(collectionName);
 
-        await collection.find(query).toArray(function(err, docs) {
+        let formattedQuery = cloneDeep(query);
+        for (const queryKey of Object.keys(formattedQuery) ) {
+            if (formattedQuery[queryKey] === 'true') {
+                formattedQuery[queryKey] = true;
+            }
+            if (parseInt(formattedQuery[queryKey], 10)) {
+                formattedQuery[queryKey] = parseInt(formattedQuery[queryKey], 10);
+            }
+        }
+
+        await collection.find(formattedQuery).toArray(function(err, docs) {
             callback(docs)
         });
         await mongoClient.close();
@@ -33,6 +44,46 @@ export async function writeDocuments(collectionName, documents, callback) {
           callback(result);
         });
         await mongoClient.close();
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+export async function updateDocument(collectionName, document) {
+    try {
+        const mongoClient = new MongoClient(mongoUri);
+        await mongoClient.connect()
+        const db = mongoClient.db(mongoDbName)
+
+        const collection = db.collection(collectionName);
+        const result = await collection.findOneAndUpdate(
+            {_id: document._id},
+            {
+                $set: document
+            },
+            {
+                upsert: true
+            }
+        )
+        await mongoClient.close();
+        return result;
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+export async function deleteDocument(collectionName, document) {
+    try {
+        const mongoClient = new MongoClient(mongoUri);
+        await mongoClient.connect()
+        const db = mongoClient.db(mongoDbName)
+
+        const collection = db.collection(collectionName);
+        const result = await collection.deleteOne(
+            {_id: document._id}
+        )
+        await mongoClient.close();
+        return result;
     } catch(err) {
         console.error(err);
     }
