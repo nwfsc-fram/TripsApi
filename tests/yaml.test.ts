82e35abf405a99e01919c698926dce57dc0534fa
Kristopher.Skelton <1603619656113305@mil>
2021-07-29 14:48:28 -0700

    restoriung test

--- tests/yaml.test.ts
+++ tests/yaml.test.ts
@@ -2,7 +2,8 @@
 test('verify test environment', () => {
     expect(true).toBe(true);
 });
-/*
+
+
 import * as fs from 'fs';
 import * as path from 'path';
 import { resolve } from 'path';
@@ -12,4 +13,3 @@ test('OpenAPI.yaml import', () => {
     const swaggerDocument = YAML.load(path.resolve(__dirname, '../openapi.yaml'));
     expect(swaggerDocument).toBeTruthy();
 })
-*/
